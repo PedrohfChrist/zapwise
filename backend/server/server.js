682ad3d9a87158eaf7c20039efa2a77ed0ee91d4
unknown <pedrohfchrist@gmail.com>
2025-03-14 19:24:03 -0300

    Atualização automática

--- backend/server/server.js
+++ backend/server/server.js
@@ -6,15 +6,12 @@ import helmet from "helmet";
 
 import chatRoutes from "./routes/chat.js";
 import userRoutes from "./routes/userRoutes.js";
-import twilioRoutes from "./routes/twilioRoutes.js";
-import twilioWebhook from "./routes/twilioWebhook.js";
 import whatsappRoutes from "./routes/whatsappRoutes.js";
 import metricsRoutes from "./routes/metricsRoutes.js";
 import manualChatRoutes from "./routes/manualChatRoutes.js";
 import automacoesRoutes from "./routes/automacoesRoutes.js";
 import knowledgeBaseRoutes from "./routes/knowledgeBaseRoutes.js";
 import fcmRoutes from "./routes/fcmRoutes.js";
-import whatsappSenderRoutes from "./routes/whatsappSenderRoutes.js";
 import facebookConnect from "./routes/facebookConnect.js";
 import facebookWebhook from "./routes/facebookWebhook.js";
 
@@ -27,8 +24,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Rotas
 app.use("/api/chat", chatRoutes);
 app.use("/api/user", userRoutes);
-app.use("/api/twilio", twilioRoutes);
-app.use("/api/twilio", twilioWebhook);
 app.use("/api/whatsapp", whatsappRoutes);
 app.use("/api/metrics", metricsRoutes);
 app.use("/api/manual-chat", manualChatRoutes);
@@ -37,9 +32,6 @@ app.use("/api/knowledge-base", knowledgeBaseRoutes);
 app.use("/api/fcm", fcmRoutes);
 app.use("/api/facebook", facebookConnect);
 app.use("/api/facebook", facebookWebhook);
-
-// AQUI: use a rota do whatsappSender
-app.use("/api/whatsapp-sender", whatsappSenderRoutes);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
